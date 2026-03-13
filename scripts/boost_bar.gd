a089e571352f8c7c2d3287e90a07cc1d22c28ea4
jyoshitanair <jyoshitanair123@gmail.com>
2026-03-12 21:32:34 -0700

    working on attack logic

--- scripts/boost_bar.gd
+++ scripts/boost_bar.gd
@@ -1,15 +1,11 @@
 extends Node2D
-#@onready var bar: Panel = $bar
-#@onready var label: Label = $Label
-#var boost
-#var fish
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#fish = get_tree().get_first_node_in_group("player")
-	#boost = fish.health
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#if fish.health != old_health:
-		#bar.size.x = fish.health * 1.8 #max of 100 makes it grand total of 180 px - the size of the bar in pxs
-		#label.text = "%s"%fish.health
+@onready var texture_progress_bar: TextureProgressBar = $TextureProgressBar
+var attack_timer
+func _ready() -> void:
+	attack_timer = get_tree().get_first_node_in_group("player").attack_timer
+	texture_progress_bar.value = 0
+func _process(delta: float) -> void:
+	#if texture_progress_bar.value == 100:
+		#texture_progress_bar.value = 0
+	texture_progress_bar.value = attack_timer.time_left*40 #value from 0-2.5, scale by 40
+	print(texture_progress_bar.value )
