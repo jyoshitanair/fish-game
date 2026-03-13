23fd9f86b78c983b0ad9d8d21c11bd15cda2f6ea
jyoshitanair <jyoshitanair123@gmail.com>
2026-03-12 21:36:25 -0700

    idk

--- scripts/boost_bar.gd
+++ scripts/boost_bar.gd
@@ -1,15 +1,3 @@
 extends Node2D
-#@onready var bar: Panel = $bar
-#@onready var label: Label = $Label
-#var boost
-#var fish
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#fish = get_tree().get_first_node_in_group("player")
-	#boost = fish.health
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#if fish.health != old_health:
-		#bar.size.x = fish.health * 1.8 #max of 100 makes it grand total of 180 px - the size of the bar in pxs
-		#label.text = "%s"%fish.health
+func _process(delta: float) -> void:
+	pass
