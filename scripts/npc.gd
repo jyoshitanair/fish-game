413008aae83ef4309426fceb1588647a32e14f61
jyoshitanair <jyoshitanair123@gmail.com>
2026-03-31 15:50:39 -0700

    working on debugging them npcs

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,13 +1,12 @@
 extends Node2D
 @onready var call_area: Area2D = $"call-area"
 var is_colliding_first_time = false
-@onready var path = preload("res://scenes/cutscenes/cut_scene1.tscn")
+@export var path = preload("res://scenes/cutscenes/cut_scene1.tscn")
 @onready var level_root: Node2D = $".."
 var is_current = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready() -> void:
 	if self.is_in_group("npc2"):
-		print("ITS REALL THE ONE PEICE")
 		is_current = true
 func _process(_delta: float) -> void:
 	##FINDING PLAYER INTERACTIONS
@@ -16,8 +15,6 @@ func _process(_delta: float) -> void:
 		if body.is_in_group("player"):
 			print(body.name)
 			dialog_enter()
-
-
 func _on_alertarea_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		print("mc detected")
