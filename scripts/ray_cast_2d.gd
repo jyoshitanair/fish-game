e53124b49246897deb5a4625597bfa44a3be332e
Jyoshita <jyoshitanair123@gmail.com>
2026-04-03 16:13:14 -0700

    fixed the flipping issues. now why is nothing working T-T

--- scripts/ray_cast_2d.gd
+++ scripts/ray_cast_2d.gd
@@ -1,6 +1,6 @@
 extends RayCast2D
 var player 
-var can_see = false
+@export var can_see = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- scripts/shark_music.gd
+++ scripts/shark_music.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 var target_pos = null
 var speed = 0.6
 var can_lerp = false
@@ -25,54 +25,52 @@ func _ready() -> void:
 	timer.wait_time = randf_range(0.3,0.5)
 	Manager.connect("change_pos",_change_target_pos)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#if can_lerp:                                          
-		#if is_attacking and raycast.can_see:  
-			#attack_timer += delta
-			#i += (1.0 - i)*delta*0.07
-			#global_position = lerp(global_position, attack_position, i)
-			#if abs(global_position.distance_to(attack_position))<=20 || attack_timer >= 7.00:
-				##attack_animation_play = true
-				#is_attacking = false	
-				#direction = (player.global_position - global_position).normalized()
-				#retreating = true
-				#first_time = false
-		#elif retreating and raycast.can_see: 
-			#direction = (player.global_position - global_position).normalized()
-			#global_position = lerp(global_position,start_position,1 - exp(-6 *delta))
-			#if global_position.distance_to(start_position) <15:
-				#global_position = start_position
-				#retreating = false
-		#else: 
-			#global_position = lerp(global_position,target_pos,1 - exp(-6 *delta))
-	if raycast.can_see:
-		print("i can see")
+func _process(delta: float) -> void: 
+	if target_pos and can_hear:
+		if global_position.distance_to(target_pos)< 10.0:
+			global_position = target_pos
+			can_lerp = false      
+	if raycast.can_see and player:    
 		direction = (player.global_position - global_position).normalized()
 		var distance = global_position.distance_to(player.global_position)
 		speed = clamp(3.0/distance *400,0.3,2.5)
-		cur_speed = lerp(cur_speed,speed,delta*2)
-		global_position = lerp(global_position, player.global_position, 1 - exp(-cur_speed *delta))
-			##FLIPPING CALCS
-		var flipper = player.global_position - global_position # negative means on the left, positive means on the right
-		old_flip = flipper.x > 5.0
+		cur_speed = lerp(cur_speed,speed,delta*2)   
 		##ATTACK LOGIC
 		if distance <= 600.0 and not is_attacking and not retreating:
 			if first_time: 
 				start_position = global_position
-			print("hi")
+				first_time = false
 			attack_timer = 0.0
 			is_attacking = true
-			attack_position =player.global_position +Vector2(-sign(scale.x) *15, 0)
-			i = 0.1              
-			
+			attack_position =player.global_position +Vector2(sign(scale.x) *15, 0)
+			i = 0.1                           
+		if is_attacking:  
+			attack_timer += delta
+			i += (1.0 - i)*delta*0.07
+			global_position = lerp(global_position, attack_position, i)
+			if global_position.distance_to(attack_position)<=20 || attack_timer >= 7.00:
+				#attack_animation_play = true
+				global_position = start_position
+				is_attacking = false	
+				direction = (player.global_position - global_position).normalized()
+				retreating = true
+				first_time = false
+		elif retreating: 
+			direction = (player.global_position - global_position).normalized()
+			global_position = lerp(global_position,start_position,1 - exp(-6 *delta))
+			if global_position.distance_to(start_position) <15:
+				global_position = start_position
+				retreating = false
+		else:
+			direction = (player.global_position - global_position).normalized()
+			global_position = lerp(global_position,player.global_position,1 - exp(-6 *delta))
+		##FLIPPING CALCS
+		var flipper = player.global_position - global_position # negative means on the left, positive means on the right
+		old_flip = flipper.x > 5.0            	
 		if flip != old_flip: 
-			scale.x = - abs(scale.x)
+			print(self)
+			self.scale.x *= -1
 		flip = old_flip
-	if target_pos != null and can_hear:
-		if global_position.distance_to(target_pos)< 10.0:
-			global_position = target_pos
-			#start_position = global_position
-			can_lerp = false
 func _change_target_pos(new_pos):
 	print("hi")
 	timer.start()
@@ -81,8 +79,6 @@ func _change_target_pos(new_pos):
 func _on_timer_timeout() -> void:
 	timer.wait_time = randf_range(0.3,0.5)
 	can_lerp = true
-	print("you can lerp now")
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("shell"):
 		can_hear = true
